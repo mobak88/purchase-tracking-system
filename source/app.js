2b280e60536f7a8e9ef9baacb27dcf3cd37cc19c
mobak88 <mostafa0401@ymail.com>
2022-11-11 10:54:12 +0100

    Testing getting all cards

--- source/app.js
+++ source/app.js
@@ -7,6 +7,21 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json()); // Access to req.body
 
+app.get('/', (req, res) => {
+    res.send('Hello, World');
+});
+
+
+//Get all cards
+app.get('/cards', async (req, res) => {
+    try {
+        const allCards = await pool.query('SELECT * FROM creditcard');
+        res.json(allCards.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.listen(8080, () => {
     console.log('Server started on port 8080');
-});
+}); 
