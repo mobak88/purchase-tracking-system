071cdff237ca3707dcbdee64d6556a225b58318d
mobak88 <mostafa0401@ymail.com>
2022-11-11 10:26:34 +0100

    Setting up express app

---
+++ source/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Access to req.body
+
+app.listen(8080, () => {
+    console.log('Server started on port 8080');
+});
