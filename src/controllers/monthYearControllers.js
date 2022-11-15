c749d054fcc4d5aa0dabd23781733a40ee82fe7f
mobak88 <mostafa0401@ymail.com>
2022-11-15 21:40:48 +0100

    Finishing GET month/year

--- src/controllers/monthYearControllers.js
+++ src/controllers/monthYearControllers.js
@@ -1,5 +1,6 @@
 /* HTTP requests exported for month/year */
 const pool = require('../db');
+const { dataStructure } = require('./../dataStructure');
 
 exports.getMonthYear = ('/months/:month_number/:year_number', async (req, res) => {
     try {
@@ -8,7 +9,9 @@ exports.getMonthYear = ('/months/:month_number/:year_number', async (req, res) =
 
         const allData = await pool.query('SELECT * FROM product JOIN transaction ON transaction_id = fk_transaction JOIN creditcard ON card_id = fk_card WHERE substring(date_string from 4 for 8) = $1', [monthAndYear]);
 
-        res.json(allData.rows);
+        const result = dataStructure(allData.rows);
+
+        res.json(result);
     } catch (err) {
         console.error(err.message);
     }
