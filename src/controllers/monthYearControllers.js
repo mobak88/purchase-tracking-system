88f6919e185b3d87207259251902edef0ae828ab
mobak88 <mostafa0401@ymail.com>
2022-11-15 21:00:29 +0100

    Working on get month/year

--- src/controllers/monthYearControllers.js
+++ src/controllers/monthYearControllers.js
@@ -1,11 +1,14 @@
 /* HTTP requests exported for month/year */
-//const pool = require('../db');
+const pool = require('../db');
 
 exports.getMonthYear = ('/months/:month_number/:year_number', async (req, res) => {
     try {
         const { month_number, year_number } = req.params;
-        const month = month_number + year_number;
-        res.json(month);
+        const monthAndYear = `${month_number}.${year_number}`;
+
+        const allData = await pool.query('SELECT * FROM product JOIN transaction ON transaction_id = fk_transaction JOIN creditcard ON card_id = fk_card WHERE substring(date_string from 4 for 8) = $1', [monthAndYear]);
+
+        res.json(allData.rows);
     } catch (err) {
         console.error(err.message);
     }
